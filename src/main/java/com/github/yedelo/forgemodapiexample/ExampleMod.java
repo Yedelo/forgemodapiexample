37fbd9ff75e7592ea5bd2d1eaf45c1521f4781bf
GitHub Action <action@github.com>
2024-04-14 03:31:36 +0000

    Initialize template repository proper
    Set package name to com.github.yedelo.forgemodapiexample
    Set project name to forgemodapiexample
    Set modid to forgemodapiexample

--- src/main/java/com/github/yedelo/forgemodapiexample/ExampleMod.java
+++ src/main/java/com/github/yedelo/forgemodapiexample/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.yedelo.forgemodapiexample;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "forgemodapiexample", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
